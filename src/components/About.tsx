7910ec4a23362df4aba8b232fddf7d4fa4a7ebda
timshaww <time4shaw@gmail.com>
2024-04-26 14:52:31 -0400

    Reverting

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,7 @@
 const About = () => {
 	return (
 		<div className='flex flex-col h-2/3 items-center justify-center mx-auto'>
-			<div className='border-black border-4 p-4 rounded-lg text-4xl'>
+			<div className='border-black border-4 p-4 rounded-lg text-4xl w-full'>
 				About Me:
 			</div>
 		</div>
