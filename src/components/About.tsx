db0db4d1752a024b0bb7f257e90dac87d94d202f
timshaww <time4shaw@gmail.com>
2024-04-26 18:31:46 -0400

    favicon + about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,8 @@
 const About = () => {
 	return (
 		<div className='flex flex-col h-2/3 items-center justify-center mx-auto'>
-			<div className='border-black border-4 p-4 rounded-lg text-4xl w-full'>
-				About Me:
+			<div className=' flex justify-center border-black border-4 p-4 rounded-lg text-4xl w-[90%] mx-4'>
+				About Me
 			</div>
 		</div>
 	);
