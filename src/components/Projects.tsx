d1e4bc4129debfdb9d570a8637bdbe32c0bd96b6
timshaww <time4shaw@gmail.com>
2025-01-23 14:10:13 -0500

    updates to projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,9 +8,9 @@ const Projects = () => {
 			<PageSection>
 				<PageHeader>Projects</PageHeader>
 			</PageSection>
-			<PageSection>
-				<div>Projects coming soon...</div>
-				{/* <div className='grid grid-cols-2 gap-4'>
+			{/* <PageSection> */}
+			<div className='flex w-full justify-center items-center p-2 mt-6'>Projects coming soon...</div>
+			{/* <div className='grid grid-cols-2 gap-4'>
 					<div className='border-zinc-200 border-[1px] rounded-lg shadow p-2'>
 						<h2 className='text-2xl'>Project 1</h2>
 						<p>Project 1 description</p>
@@ -28,7 +28,7 @@ const Projects = () => {
 						<p>Project 4 description</p>
 					</div>
 				</div> */}
-			</PageSection>
+			{/* </PageSection> */}
 		</PageLayout>
 	);
 };
